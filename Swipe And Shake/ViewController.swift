738223363a67d16e527e3a0988b3c9e259895b05
mn <pesrobot@gmail.com>
2016-05-26 22:52:39 +0300

    initial commit

--- Swipe And Shake/ViewController.swift
+++ Swipe And Shake/ViewController.swift
@@ -7,14 +7,38 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player:AVAudioPlayer = AVAudioPlayer()
+    
+    var sounds = ["beep1","beep2","beep3","beep4","beep11","beep12","beep13"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    
+    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent?) {
+        if event?.subtype == UIEventSubtype.MotionShake{
+            
+            var randomNumber = Int(arc4random_uniform(UInt32(sounds.count)))
+            
+            
+            var fileLocation = NSBundle.mainBundle().pathForResource(sounds[randomNumber], ofType: "mp3")
+            
+            do {try
+                player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: fileLocation!))
+                player.play()
+            }catch{
+                //print sth
+            }
+            
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
